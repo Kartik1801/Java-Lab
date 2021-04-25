9614914bbb601fc7d82d2e59568483b17df26ec4
KD1801 <kartikdhawan1801@gmail.com>
2021-04-25 22:59:33 +0530

    Added palindrome.java | Checks if a no is palindrome or not.

---
+++ 1-2-palindrome.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class palindrome{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);//System.in is standard input stream
+        int no=sc.nextInt();
+        int rev=0,rem,copy=no;
+        while (copy!=0){
+            rem=copy%10;
+            rev=rev*10+rem;
+            copy/=10;
+        }
+        if(rev==no)
+        System.out.println("It is a Palindrome");
+        else
+        System.out.println("It is not a Palindrome");
+    }
+}
