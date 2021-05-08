583a988ace5aa593e79065095f70a510ff084ce8
KD1801 <kartikdhawan1801@gmail.com>
2021-05-08 13:52:31 +0530

    Added lab 2-1 and 2-2

---
+++ 2-1-widening-typecast.java
@@ -0,0 +1,10 @@
+class wtc
+{
+    public static void main(String args[])
+    {
+        int num=10;
+        System.out.println("The double value is : "+num);
+        double data=(double) num;
+        System.out.println("The int value is : "+data);
+    }
+}
---
+++ 2-2-narrowing-typecasting.java
@@ -0,0 +1,10 @@
+class ntc
+{
+    public static void main(String args[])
+    {
+        double data=10.33;
+        System.out.println("The double value is : "+data);
+        int num=(int) data;
+        System.out.println("The int value is : "+num);
+    }
+}
