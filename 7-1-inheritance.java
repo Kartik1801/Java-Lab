8683dd70efa3769a0615871205edd35161808007
KD1801 <kartikdhawan1801@gmail.com>
2021-06-19 14:01:42 +0530

    added 7-1

---
+++ 7-1-inheritance.java
@@ -0,0 +1,28 @@
+import jdk.internal.vm.PostVMInitHook;
+
+class calc{
+int  r;
+public void sum(int a,int b){
+    r=a+b;
+    System.out.println("sum = "+r);
+}
+public void sub(int a,int b){
+    r=a-b;
+System.out.println("difference = "+r);
+}
+}
+class myClass extends calc {
+    public void mul(int a,int b){
+        r=a*b;
+        System.out.println("product = "+r);
+    }
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        myClass m= new myClass();
+        m.sum(a, b);
+        m.sub(a, b);
+        m.mul(a, b);
+
+    }
+}
